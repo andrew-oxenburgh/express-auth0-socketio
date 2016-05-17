38e9ed9adb8ebfe2a220e31c99b7e4ee540342aa
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2016-05-18 10:06:57 +1200

    has public, private and sign-on api points

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var debug = require('debug')('passport-tut:index');
+
+var port = process.env.PORT || 5050;
+
+app.set('port', port);
+
+app.get('/', function(req, resp){
+   resp.send('hello, world');
+});
+
+app.get('/public', function(req, resp){
+   resp.send('public');
+});
+
+app.get('/private', function(req, resp){
+   resp.send('private');
+});
+
+app.get('/sign-on', function(req, resp){
+   resp.send('sign-on');
+});
+
+app.listen(app.get('port'), imListening);
+exports.port = port;
+exports.app = app;
+
+debug('starting up');
+
+function imListening() {
+   console.log('running');
+   debug('Node app is running on port', app.get('port'));
+   debug('versions:', process.versions);
+   console.log('im listening');
+}
+
