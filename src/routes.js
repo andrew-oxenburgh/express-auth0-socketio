7c8e8e3394f2cac1aad909c31fd2b48f28884bf2
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2016-05-26 13:45:21 +1200

    formatting

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ var passport = require('passport');
 var bluebird = require('bluebird');
 var jwt = bluebird.promisifyAll(require('jsonwebtoken'));
 
-var redirectIfUnauthed = function (req, resp, next) {
+var redirectIfUnauthed = function(req, resp, next) {
    if (req.isAuthenticated()) {
       next();
    } else {
@@ -13,10 +13,10 @@ var redirectIfUnauthed = function (req, resp, next) {
    }
 };
 
-var privatePage = function (req, res) {
+var privatePage = function(req, res) {
    var encrypted_token = req.user.encrypted_jwt_token;
    jwt.verifyAsync(encrypted_token, process.env.JWT_TOKEN_SECRET)
-      .then(function () {
+      .then(function() {
          res.render('private.ejs',
             {
                jwt_token: encrypted_token,
@@ -24,7 +24,7 @@ var privatePage = function (req, res) {
                jwt_timeout: process.env.JWT_TOKEN_TIMEOUT || '7days'
             });
       })
-      .catch(function (err) {
+      .catch(function(err) {
          // timed out, or something
          req.logout();
          res.locals.timeout = true;
@@ -32,7 +32,7 @@ var privatePage = function (req, res) {
       });
 };
 
-var publicPage = function (req, res) {
+var publicPage = function(req, res) {
    res.render('public.ejs',
       {
          authed: req.isAuthenticated(),
@@ -40,18 +40,18 @@ var publicPage = function (req, res) {
       });
 };
 
-var logout = function (req, res, next) {
+var logout = function(req, res, next) {
    req.logout();
    next();
 };
 
-var timeout = function (req, res, next) {
+var timeout = function(req, res, next) {
    req.logout();
    res.locals.timeout = true;
    next();
 };
 
-module.exports = function (app) {
+module.exports = function(app) {
    app.get('/', publicPage);
 
    app.get('/private',
@@ -73,7 +73,7 @@ module.exports = function (app) {
       passport.authenticate('auth0', {
          failureRedirect: '/'
       }),
-      function (req, res, next) {
+      function(req, res, next) {
          debug('/callback', req.isAuthenticated());
          if (!req.user) {
             throw new Error('user null');
